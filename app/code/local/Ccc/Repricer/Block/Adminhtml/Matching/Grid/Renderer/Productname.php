9a530ff0bd78265b3701d8c483b8950aeea79358
Parth Tandel <115548946+ParthTandel007@users.noreply.github.com>
2024-04-24 03:34:18 +0530

    renderer for product_name

--- app/code/local/Ccc/Repricer/Block/Adminhtml/Matching/Grid/Renderer/Productname.php
+++ app/code/local/Ccc/Repricer/Block/Adminhtml/Matching/Grid/Renderer/Productname.php
@@ -2,27 +2,17 @@
 
 class Ccc_Repricer_Block_Adminhtml_Matching_Grid_Renderer_Productname extends Mage_Adminhtml_Block_Widget_Grid_Column_Renderer_Abstract
 {
-    // Use a static variable to track rendered product IDs
     protected static $renderedProductIds = [];
-
     public function render(Varien_Object $row)
     {
-        $productId = $row->getData('product_id'); 
+        $productId = $row->getData('product_id');
         $productName = $row->getData('product_name');
         $productSku = $row->getData('product_sku');
-
-        if (in_array($productId, self::$renderedProductIds)) {
-            return '';
-        }
-
+        
         self::$renderedProductIds[] = $productId;
-
-        $displayText = $productName;
-        if (!empty($productSku)) {
-            $displayText .= ' (' . $productSku . ')';
-        }
-        return $displayText;
+        return "<span><b>Product Name: </b>". $productName ." <br> <b>Product Sku: </b>". $productSku ." </span>";
     }
 }
+
 
 ?>
