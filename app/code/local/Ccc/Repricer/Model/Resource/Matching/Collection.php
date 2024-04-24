b3cbbf7456e4ba70cc51324b4f8fe50a6cd80ae1
Parth Tandel <115548946+ParthTandel007@users.noreply.github.com>
2024-04-24 18:31:31 +0530

    getSelectCountSql function add

--- app/code/local/Ccc/Repricer/Model/Resource/Matching/Collection.php
+++ app/code/local/Ccc/Repricer/Model/Resource/Matching/Collection.php
@@ -5,6 +5,25 @@ class Ccc_Repricer_Model_Resource_Matching_Collection extends Mage_Core_Model_Re
     {
         $this->_init('ccc_repricer/matching');
     }
+    // public function getSize(){
+    //     return count($this->getAllIds());
+    // }
+    public function getSelectCountSql()
+    {
+        $this->_renderFilters();
+
+        $unionSelect = clone $this->getSelect();
+
+        $unionSelect->reset(Zend_Db_Select::ORDER);
+        $unionSelect->reset(Zend_Db_Select::LIMIT_COUNT);
+        $unionSelect->reset(Zend_Db_Select::LIMIT_OFFSET);
+
+        $countSelect = clone $this->getSelect();
+        $countSelect->reset();
+        $countSelect->from(array('a' => $unionSelect), 'COUNT(*)');
+
+        return $countSelect;
+    }
 }
 
 ?>
